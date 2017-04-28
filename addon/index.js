4ee321e3d5dfff9acf14e6982dc14c291e9c60ad
Stefan Penner <stefan.penner@gmail.com>
2017-04-28 10:24:05 -0700

    Update index.js

--- addon/index.js
+++ addon/index.js
@@ -1,6 +1,10 @@
 export default class {
   static create(attrs) {
     return new this(attrs);
+  } 
+  
+  static extend() {
+    throw new Error('please use ES6 class syntax for subclassing this object');
   }
 
   get(key) {
