13a2beed73510b54b3bf9b8bcd259e709d1ab5b2
Robert Jackson <me@rwjblue.com>
2018-05-16 16:08:48 -0400

    Remove `this.get` / `this.set`.
    When using decorators (for CP support) `this.get` / `Ember.get` is not needed (we setup valid normal getters in Ember 3.1)...

--- addon/index.js
+++ addon/index.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+
 export default class {
   constructor() {
     Ember.meta(this); 
@@ -10,13 +11,5 @@ export default class {
   
   static extend() {
     throw new Error('please use ES6 class syntax for subclassing this object');
-  }
-
-  get(key) {
-    return Ember.get(this, key);
-  }
-
-  set(key) {
-    return Ember.set(this, key);
   }
 }
